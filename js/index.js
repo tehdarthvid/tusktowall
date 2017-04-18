b8d21734774aa95aba6bdf1822d1cf8978414880
darthvid <darthvid@hotmail.com>
2017-04-19 02:38:35 +0800

    NSFW button now more descriptive.

--- js/index.js
+++ js/index.js
@@ -118,11 +118,11 @@ function toggleNSFW() {
 function setVisibilityNSFW(isVisible) {
 	if (isVisible) {
 		$(".imgNSFW").css("visibility", "visible");
-		$("#btnNSFW").html("#NSFW");
+		$("#btnNSFW").html("swaitch to supposedly #SFW");
 		$("#btnNSFW").css("color", "red");
 	} else {
 		$(".imgNSFW").css("visibility", "hidden");
-		$("#btnNSFW").html('supposedly "#sfw"');
+		$("#btnNSFW").html('swtich to #nsfw');
 		$("#btnNSFW").css("color", "black");
 	}
 }
